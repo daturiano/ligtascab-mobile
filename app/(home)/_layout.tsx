3203f000f124a64477fed43db48acf159ce9d371
danturiano <dturiano.r@gmail.com>
2025-04-20 23:04:25 +0800

    refactor: add react native reusables

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,84 +1,77 @@
 import { HapticTab } from '@/components/HapticTab';
 import TabBarBackground from '@/components/ui/TabBarBackground';
+import { NAV_THEME } from '@/lib/constants';
 import { Tabs } from 'expo-router';
 import { Bike, CircleUser, Heart, Search } from 'lucide-react-native';
 import React from 'react';
-import { Platform, Text, View } from 'react-native';
+import { Platform, useColorScheme } from 'react-native';
 
 export default function TabLayout() {
+  const colorScheme = useColorScheme();
   return (
-    <View className="flex-1">
-      <View className="flex items-center justify-center pt-16 bg-white">
-        <Text className="text-xl font-bold text-gray-900">ligtascab.</Text>
-      </View>
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: '#1daa88',
-          headerShown: false,
-          tabBarButton: HapticTab,
-          tabBarBackground: TabBarBackground,
-          tabBarStyle: Platform.select({
-            ios: {
-              // Use a transparent background on iOS to show the blur effect
-              position: 'absolute',
-            },
-            default: {},
-          }),
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: NAV_THEME[colorScheme ?? 'light'].primary,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: 'absolute',
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Search
+              color={color}
+              size={size}
+              strokeWidth={focused ? 2.5 : 1.5}
+            />
+          ),
         }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: 'Search',
-            tabBarIcon: ({ color, size, focused }) => (
-              <Search
-                color={color}
-                size={size}
-                strokeWidth={focused ? 2.5 : 1.5}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="saved"
-          options={{
-            title: 'Saved',
-            tabBarIcon: ({ color, size, focused }) => (
-              <Heart
-                color={color}
-                size={size}
-                fill={focused ? color : '#ffffff'}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="trips"
-          options={{
-            title: 'Trips',
-            tabBarIcon: ({ color, size, focused }) => (
-              <Bike
-                color={color}
-                size={size}
-                strokeWidth={focused ? 2.5 : 1.5}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="profile"
-          options={{
-            title: 'Profile',
-            tabBarIcon: ({ color, size, focused }) => (
-              <CircleUser
-                color={color}
-                size={size}
-                strokeWidth={focused ? 2.5 : 1.5}
-              />
-            ),
-          }}
-        />
-      </Tabs>
-    </View>
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: 'Saved',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Heart
+              color={color}
+              size={size}
+              fill={focused ? color : '#ffffff'}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="trips"
+        options={{
+          title: 'Trips',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Bike color={color} size={size} strokeWidth={focused ? 2.5 : 1.5} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, size, focused }) => (
+            <CircleUser
+              color={color}
+              size={size}
+              strokeWidth={focused ? 2.5 : 1.5}
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,41 @@
+import { useColorScheme } from '@/lib/useColorScheme';
+import {
+  DarkTheme,
+  DefaultTheme,
+  Theme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import 'react-native-reanimated';
+import { NAV_THEME } from '~/lib/constants';
 import './global.css';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
 
 export default function RootLayout() {
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+
   return (
-    <>
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
       <Stack>
         <Stack.Screen name="(home)" options={{ headerShown: false }} />
       </Stack>
-      <StatusBar style="auto" />
-    </>
+    </ThemeProvider>
   );
 }
